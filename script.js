25484ed68219e2597850eb9bf195db5072612b5d
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-12-18 16:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click",Increment);
+
+function Increment() {
+	let val = document.getElementById("counter");
+	let val2 = parseInt(val.textContent);
+	alert(val2);
+	val.textContent = val2+1;
+}
